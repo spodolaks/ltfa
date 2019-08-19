c7078f4569f8d3a5ff440f3d954ec744d06a5ea0
edgar <edgar.jegorov@claritylabs.eu>
2019-08-19 16:16:45 +0300

    base pages

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('news/', views.news, name='news'),
+    path('tournaments/', views.tournaments, name='tournaments'),
+    path('events/', views.events, name='events'),
+    path('documents/', views.documents, name='documents'),
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('eshop/', views.eshop, name='eshop'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,27 @@ from django.template import loader
 def index(request):
     template = loader.get_template('app/index.html')
     return HttpResponse(template.render({}, request))
+
+def news(request):
+    template = loader.get_template('app/news.html')
+    return HttpResponse(template.render({}, request))
+
+def tournaments(request):
+    template = loader.get_template('app/tournaments.html')
+    return HttpResponse(template.render({}, request))
+
+def events(request):
+    template = loader.get_template('app/events.html')
+    return HttpResponse(template.render({}, request))
+
+def documents(request):
+    template = loader.get_template('app/documents.html')
+    return HttpResponse(template.render({}, request))
+
+def aboutus(request):
+    template = loader.get_template('app/aboutus.html')
+    return HttpResponse(template.render({}, request))
+
+def eshop(request):
+    template = loader.get_template('app/eshop.html')
+    return HttpResponse(template.render({}, request))
