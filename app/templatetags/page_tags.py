fb9ba95ded9c45ba3eec346bb83395688e574f5b
edgar <edgar.jegorov@claritylabs.eu>
2019-09-05 17:27:15 +0300

    pagination

--- app/templatetags/page_tags.py
+++ app/templatetags/page_tags.py
@@ -25,3 +25,46 @@ def page_by_layout(layout):
         return Page.objects.get(layout='templates/app/'+layout+'.html')
     except Content.DoesNotExist:
         return None
+
+@register.simple_tag
+def compose_page_number(num=""):
+    try:
+        result = int(num)
+        if result > 0:
+            return result;
+        else:
+            return 1;
+    except:
+        return 1;
+
+@register.simple_tag
+def pagination(count=1, current=1, url="?page={0}"):
+    offset = 3
+    result = [];
+    if count > 1:
+        if current > 1:
+            result.append({
+                'url': url.format(current - 1),
+                'title': "<",
+            });
+        dots_added = False;
+        for i in range(1, count + 1):
+            print(dots_added);
+            if i <= offset or i > count - offset or (i > current - offset and i < current + offset):
+                dots_added = False;
+                result.append({
+                    'url': url.format(i),
+                    'active': i == current,
+                    'title': i,
+                });
+            elif dots_added == False:
+                dots_added = True;
+                result.append({
+                    'title': ".&nbsp;.&nbsp;.",
+                });
+        if current < count:
+            result.append({
+                'url': url.format(current + 1),
+                'title': ">",
+            });
+    return result;
