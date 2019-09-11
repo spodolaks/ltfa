7f8078c9064fe22e40468495d7bfe7542986ac2c
edgar <edgar.jegorov@claritylabs.eu>
2019-09-11 18:23:37 +0300

    refactorin

--- app/admin.py
+++ app/admin.py
@@ -14,38 +14,28 @@ class PageAdmin(admin.ModelAdmin):
             path('order/dec/<int:pk>/', self.dec),
         ]
         return my_urls + urls
-        
-    def inc(self, request, pk):
+
+    def order(self, request, pk, inc=True):
         page = Page.objects.get(pk=pk);
         if page:
-            pages = Page.objects.filter(parent=page.parent).order_by('-order');
+            pages = Page.objects.filter(parent=page.parent).order_by('-order' if inc else 'order');
             prev_page = None;
             for p in pages:
-                if p.order > page.order:
+                cond = (p.order > page.order) if inc else (p.order < page.order)
+                if cond:
                     prev_page = p;
-                else:
-                    break;
+                else: break;
             if prev_page:
                 prev_page.order, page.order = page.order, prev_page.order
                 page.save(reorder=False);
                 prev_page.save();
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+    def inc(self, request, pk):
+        return self.order(request, pk, inc=True);
+
     def dec(self, request, pk):
-        page = Page.objects.get(pk=pk);
-        if page:
-            pages = Page.objects.filter(parent=page.parent).order_by('order');
-            prev_page = None;
-            for p in pages:
-                if p.order < page.order:
-                    prev_page = p;
-                else:
-                    break;
-            if prev_page:
-                prev_page.order, page.order = page.order, prev_page.order
-                page.save(reorder=False);
-                prev_page.save();
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        return self.order(request, pk, inc=False);
 
 admin.site.register(Text)
 admin.site.register(Social)
