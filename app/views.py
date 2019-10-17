80169876ef7b4c999dbe81a50220f472dba0a39b
Edgar Jegorv <jegorov.e@gmail.com>
2019-10-17 21:06:30 +0300

    events

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.forms.models import model_to_dict
 from .models import Page, News
 from .utils.api import Api
-from .utils.api_models import MatchesTable, Calendar, Facilities
+from .utils.api_models import Matches, MatchesTable, Calendar, Facilities, Facility
 
 def pages(request, url=''):
     if url == '':
@@ -17,6 +17,42 @@ def pages(request, url=''):
         return globals()[layout](request, p)
     else:
         return render(request, p.layout, model_to_dict(p));
+
+def events(request, page):
+    data = model_to_dict(page);
+    api = Api()
+    seasons = api.get_seasons().list;
+    current_season = request.GET.get("season");
+    if current_season not in seasons:
+        current_season = next(iter(seasons))
+    competitions = api.get_competitions(current_season);
+    matches = [];
+    for comp in competitions.list:
+        matches_list = api.get_matches(comp.competitionFifaId).list
+        teams = api.get_teams(comp.competitionFifaId);
+        for m in matches_list:
+            matches.append({
+                "match": m,
+                "competition": comp,
+                "teams":[
+                    teams.list[m.matchTeams.getHomeID()],
+                    teams.list[m.matchTeams.getAwayID()]
+                ],
+                "facility": Facility(api.get_facility(m.facilityFifaId))
+            })
+
+    complete_matches = [x for x in matches if x["match"].status == "PLAYED"]
+    next_matches = [x for x in matches if x["match"].status != "PLAYED"]
+    complete_matches.sort(key=lambda x:x["match"].date)
+    next_matches.sort(key=lambda x:x["match"].date)
+    data.update({
+        "current_season": current_season,
+        "seasons": seasons,
+        "competitions": competitions.list,
+        "complete_matches": complete_matches,
+        "next_matches": next_matches
+    })
+    return render(request, page.layout, data);
 
 def tournaments(request, page):
     data = model_to_dict(page);
